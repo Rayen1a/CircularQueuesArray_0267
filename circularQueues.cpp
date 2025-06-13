9b1710f181922fb47d1864649b00fb4eec65a1be
Rayen1a <raihan01426@gmail.com>
2025-06-13 08:41:47 +0700

    mengecek apakah antrian kosong

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -63,5 +63,11 @@ class Queues {
     void display() {
         int FRONT_position = FRONT;
         int REAR_position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty/n";
+            return;
+        }
     }
-}
+};
